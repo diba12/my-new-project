71bda461b0cee0b89b0b835680903a2c31a59abd
diba12 <49678851+diba12@users.noreply.github.com>
2019-06-13 10:34:00 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+ int mYlist_ [] = {11,10,9,8,7,6,5,4,3,2,1} ;
+ for (int i=0; i<11; i++)
+ {
+     cout << mYlist_[i] <<endl;
+ }
+    return 0;
+}
+
